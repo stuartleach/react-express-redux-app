ece249219452b23ce71e802827d9269cda123b2e
Stu Leach <jstuartleach@gmail.com>
2021-10-11 07:55:25 -0400

    Remove non-working autocomplete

--- client/src/component/EditForm.js
+++ client/src/component/EditForm.js
@@ -17,26 +17,15 @@ export default function EditForm(props) {
 	const state = useSelector((state) => state.editForm)
 	const globalState = useSelector((state) => state)
 
-	// const [currentId, setCurrentId] = useState(props.clickedId - 1)
 	const currentId = state.currentId
 	const titleRef = createRef(null)
 	const bodyRef = createRef(null)
 
 	let results = props.results
 
-	const options = [
-		{ value: 'js', label: 'js' },
-		{ value: 'jds', label: 'jds' },
-	]
-
-	const [result, setResult] = useState([])
-
 	return (
 		<form>
 			<Center>
-				{/* {Object.entries(results[currentId]).map((x) => (
-					<div>{x}</div>
-				))} */}
 				<FormControl>
 					<FormLabel htmlFor='name'>First name</FormLabel>
 					<Box>
@@ -46,25 +35,10 @@ export default function EditForm(props) {
 						<Text>id: {results[currentId].id}</Text>
 					</Box>
 					<Box>
-						{/* <Input
+						<Input
 							id='title'
 							placeholder={results[currentId].title}
 							ref={titleRef}
-							onChange={() =>
-								dispatch({
-									type: 'EDIT_TITLE',
-									results: props.results,
-									title: titleRef.current.value,
-									clickedId: props.clickedId,
-								})
-							}
-						/> */}
-						<Autocomplete
-							id='title'
-							placeholder={results[currentId].title}
-							ref={titleRef}
-							options={options}
-							setResult={(options) => setResult(options)}
 							onChange={() =>
 								dispatch({
 									type: 'EDIT_TITLE',
